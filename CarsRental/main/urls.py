9c4245c9625fe2564fe7755056b1ea89262ab95e
Hesham <HeshamAlsadan@gmail.com>
2025-07-06 17:29:43 +0300

    lab and bonus

--- CarsRental/main/urls.py
+++ CarsRental/main/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.home_view, name="home_view"),
     path("about/", views.about_view, name="about_view"),
+    path("password/generate/", views.gen_password, name="gen_password")
 ]
--- CarsRental/main/views.py
+++ CarsRental/main/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+import random
+import string
 
 # Create your views here.
-def home_view(requste: HttpRequest):
+def home_view(request : HttpRequest):
 
     contant = "Hello World, This is my new HOME for Car Rentals Website  ! we're excited to welcome you here."
     return HttpResponse(contant)
 
 
-def about_view(requste: HttpRequest):
+def about_view(request : HttpRequest):
     contant = "A simple paragraph about Car Rentals."
     return HttpResponse(contant)
+
+def gen_password(request : HttpRequest):
+    numbers = random.sample(string.digits, 2)
+    upper = random.sample(string.ascii_uppercase, 3)
+    lower = random.sample(string.ascii_lowercase, 3)
+    symbols = random.sample(string.punctuation, 2)
+    password = numbers + upper + lower + symbols
+    random.shuffle(password)
+    shuffle_password = ''.join(password)
+    return HttpResponse(shuffle_password)
+
+
+
+    
